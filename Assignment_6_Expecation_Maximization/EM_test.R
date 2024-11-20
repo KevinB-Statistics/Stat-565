ba690b4fa09d877a902a00bbd0edc7a7b1e48354
Kevin Bui <kevin.bui100@outlook.com>
2024-11-19 16:34:40 -0800

    changed output to lists

--- Assignment_6_Expecation_Maximization/EM_test.R
+++ Assignment_6_Expecation_Maximization/EM_test.R
@@ -45,7 +45,7 @@
 ############################################################################
 # OUR ACTUAL FUNCTION
 #################################################################
-EM <- function(x, tolerance, mu, sigma, pi_gmm, max_iter=1e4){
+EM <- function(x, tolerance = 1e-6, max_iter = 1e4){
   
   if(typeof(x) != "list"){
     stop("Data must be a list")
@@ -128,9 +128,8 @@ EM <- function(x, tolerance, mu, sigma, pi_gmm, max_iter=1e4){
                       c(var_x1 = sigma[[2]][1,1], var_x2 = sigma[[2]][2,2], cov_x1x2 = sigma[[2]][1,2]),
                       c(var_x1 = sigma[[3]][1,1], var_x2 = sigma[[3]][2,2], cov_x1x2 = sigma[[3]][1,2]))
     
-  pi_gmm_final <- list(pi_1 = pi_gmm[1,], pi_2 = pi_gmm[2,], pi_3 = pi_gmm[3,])
+  pi_gmm_final <- list(pi_1 = pi_gmm[1], pi_2 = pi_gmm[2], pi_3 = pi_gmm[3])
   
-  return(c(mu_final, sigma_final, pi_gmm_final))  # This line returns the outputs as a list.
+  return(list(mu_final, sigma_final, pi_gmm_final))  # This line returns the outputs as a list.
 
 }
-
